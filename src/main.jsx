3c2ce3e81dabc9a45b34ecff62a011c33f91cdf8
Abu Bin Naser <abubinnaser81@users.noreply.github.com>
2025-08-19 21:33:14 +0600

    first firebase

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {
+   createBrowserRouter,
+   RouterProvider,
+   } from 'react-router';
+   import Root from './layouts/root.jsx'; 
+  const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: Root,
+    children: [
+    ]
+      
+      
+  }
+])
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
